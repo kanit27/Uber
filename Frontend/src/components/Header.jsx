58f57cfc129cafb515c18a3d2b4f656d04e57a3e
Kanit Gohel <kanitgohel27@gmail.com>
2025-05-16 21:05:31 +0530

    updated the changes again

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -3,9 +3,8 @@ import { IoIosSettings   } from 'react-icons/io';
 
 const Header = () => {
   return (
-    <div className="w-full h-16 flex items-center justify-between px-4">
-      <SiUber className="w-20 h-14" />
-      <IoIosSettings  className="w-8 h-8 rounded-full bg-black text-white p-1" />
+    <div className="w-full absolute flex justify-end  p-4 z-[9999]">
+      <IoIosSettings  className="w-8 h-8  rounded-full bg-black text-white p-1" />
     </div>
   );
 }
--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,3 +1,4 @@
+import Header from "../components/Header";
 import Maps from "../components/Maps";
 import WhereTo from "../components/WhereTo";
 import { useState } from "react";
