ee709103be804da6883d5ec11cfdacc22468a2d5
Kanit Gohel <kanitgohel27@gmail.com>
2025-08-14 01:25:54 +0530

    updated both socket files

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = import.meta.env.VITE_BASE_URL || "http://localhost:4000";
+const SERVER_URL = import.meta.env.VITE_BASE_URL;
 const socket = io(SERVER_URL, {
   transports: ["websocket"],
 });
